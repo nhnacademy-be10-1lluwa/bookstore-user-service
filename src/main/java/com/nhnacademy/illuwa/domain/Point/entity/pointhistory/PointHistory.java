314beae7750cadef1554cd45b29b843c380a5cb1
CGE <bestofGE@naver.com>
2025-06-09 20:37:52 +0900

    feat: point-history entity, dto 추가

---
+++ src/main/java/com/nhnacademy/illuwa/domain/Point/entity/pointhistory/PointHistory.java
@@ -0,0 +1,36 @@
+package com.nhnacademy.illuwa.domain.Point.entity.pointhistory;
+
+import com.nhnacademy.illuwa.domain.Point.entity.pointhistory.enums.PointHistoryType;
+import com.nhnacademy.illuwa.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "point_history")
+public class PointHistory {
+    @Id
+    @GeneratedValue
+    @Column(name = "point_history_id")
+    private int pointHistoryId;
+
+    @Column(nullable = false)
+    private int amount;
+
+    private String reason;
+
+    @Column(nullable = false)
+    private PointHistoryType type;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
