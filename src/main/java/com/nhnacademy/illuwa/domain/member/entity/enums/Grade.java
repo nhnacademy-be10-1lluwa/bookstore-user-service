c1b391c5e22f480bd99f637dfc2089d198e0eaea
CGE <bestofGE@naver.com>
2025-06-10 09:45:04 +0900

    remove: 잘못 업로드된 이전버전 파일 삭제

--- src/main/java/com/nhnacademy/illuwa/domain/member/entity/enums/Grade.java
+++
@@ -1,5 +0,0 @@
-package com.nhnacademy.illuwa.domain.member.entity.enums;
-
-public enum Grade {
-    비회원, 일반, 로얄, 골드, 플래티넘
-}
--- src/main/java/com/nhnacademy/illuwa/domain/member/entity/enums/Status.java
+++
@@ -1,5 +0,0 @@
-package com.nhnacademy.illuwa.domain.member.entity.enums;
-
-public enum Status {
-    ACTIVE, INACTIVE, DELETED
-}
