a5f0703d7d477318b2257687e2154da875f57781
CGE <bestofGE@naver.com>
2025-06-12 17:02:43 +0900

    refactor: 커스텀 예외 4개로 정리

--- src/main/java/com/nhnacademy/illuwa/domain/member/exception/MemberAuthenticationFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.illuwa.domain.member.exception;
-
-public class MemberAuthenticationFailedException extends RuntimeException {
-    public MemberAuthenticationFailedException() {
-        super("로그인에 실패했습니다.");
-    }
-}
