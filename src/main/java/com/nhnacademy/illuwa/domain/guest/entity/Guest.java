68893be36d432a872749a93358952765d6307b7a
CGE <bestofGE@naver.com>
2025-06-13 11:24:22 +0900

    refactor: AllArgsConstructor 제거, pk없는 생성자 생성

--- src/main/java/com/nhnacademy/illuwa/domain/guest/entity/Guest.java
+++ src/main/java/com/nhnacademy/illuwa/domain/guest/entity/Guest.java
@@ -1,36 +1,44 @@
 package com.nhnacademy.illuwa.domain.guest.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "guests")
-@Data
+@Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Guest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "guest_id")
     private long guestId;
 
-    @Column(name = "name")
+    @Setter
+    @Column(name = "name",nullable = false)
     private String name;
 
-    @Column(name = "email")
+    @Setter
+    @Column(name = "email",nullable = false)
     private String email;
 
-    @Column(name = "order_password")
+    @Setter
+    @Column(name = "order_password",nullable = false)
     private String orderPassword;
 
-    @Column(name = "contact")
+    @Setter
+    @Column(name = "contact",nullable = false)
     private String contact;
 
-    @OneToOne
-    @JoinColumn(name = "order_id")
-    private Order order;
+    @Setter
+    @JoinColumn(name = "order_id",nullable = false)
+    private long orderId;
+
+    @Builder
+    public Guest(String name, String email, String orderPassword, String contact, Long orderId) {
+        this.name = name;
+        this.email = email;
+        this.orderPassword = orderPassword;
+        this.contact = contact;
+        this.orderId = orderId;
+    }
 }
