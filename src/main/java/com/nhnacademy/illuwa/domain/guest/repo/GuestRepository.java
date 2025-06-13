de11388c6e67c96326096176d48d874825f28222
GAEUN <82525776+bestofGE@users.noreply.github.com>
2025-06-13 15:35:19 +0900

    feature/25-guest-repository (#28)
    * feat: guest repository, test code, test전용 yml 파일 생성
    
    * fix: 설정파일 sql 설정 통일

---
+++ src/main/java/com/nhnacademy/illuwa/domain/guest/repo/GuestRepository.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.illuwa.domain.guest.repo;
+
+import com.nhnacademy.illuwa.domain.guest.entity.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+    Guest findGuestByOrderIdAndOrderPassword(long orderId, String orderPassword);
+}
