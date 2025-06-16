8d3c31605c97e38f290461aac9c302a5c5bce382
CGE <bestofGE@naver.com>
2025-06-16 20:45:53 +0900

    feat: pointpolicy test 구현

--- src/main/java/com/nhnacademy/illuwa/domain/pointpolicy/service/PointPolicyService.java
+++ src/main/java/com/nhnacademy/illuwa/domain/pointpolicy/service/PointPolicyService.java
@@ -1,12 +1,14 @@
 package com.nhnacademy.illuwa.domain.pointpolicy.service;
 
+import com.nhnacademy.illuwa.domain.pointpolicy.dto.PointPolicyCreateRequest;
 import com.nhnacademy.illuwa.domain.pointpolicy.dto.PointPolicyResponse;
 import com.nhnacademy.illuwa.domain.pointpolicy.dto.PointPolicyUpdateRequest;
 
 import java.util.List;
 
 public interface PointPolicyService {
-    PointPolicyResponse getPointPolicy(String policyKey);
-    List<PointPolicyResponse> getAllPointPolicy();
+    List<PointPolicyResponse> saveAllPointPolicy(List<PointPolicyCreateRequest> requestList);
+    PointPolicyResponse findByPolicyKey(String policyKey);
+    List<PointPolicyResponse> findAllPointPolicy();
     PointPolicyResponse updatePointPolicy(String policyKey, PointPolicyUpdateRequest request);
 }
--- src/main/java/com/nhnacademy/illuwa/domain/pointpolicy/utils/PointPolicyMapper.java
+++ src/main/java/com/nhnacademy/illuwa/domain/pointpolicy/utils/PointPolicyMapper.java
@@ -1,8 +1,6 @@
 package com.nhnacademy.illuwa.domain.pointpolicy.utils;
 
-import com.nhnacademy.illuwa.domain.address.dto.AddressRequest;
-import com.nhnacademy.illuwa.domain.address.dto.AddressResponse;
-import com.nhnacademy.illuwa.domain.address.entity.Address;
+import com.nhnacademy.illuwa.domain.pointpolicy.dto.PointPolicyCreateRequest;
 import com.nhnacademy.illuwa.domain.pointpolicy.dto.PointPolicyResponse;
 import com.nhnacademy.illuwa.domain.pointpolicy.dto.PointPolicyUpdateRequest;
 import com.nhnacademy.illuwa.domain.pointpolicy.entity.PointPolicy;
@@ -12,6 +10,8 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface PointPolicyMapper {
-    PointPolicyResponse pointPolicyToDto(PointPolicy pointPolicy);
-    void updatePointPolicy(@MappingTarget PointPolicy target, PointPolicyUpdateRequest request);
+    PointPolicy dtoToEntity(PointPolicyCreateRequest dto);
+    PointPolicy dtoToEntity(PointPolicyResponse dto);
+    PointPolicyResponse entityToDto(PointPolicy pointPolicy);
+    PointPolicy updatePointPolicy(@MappingTarget PointPolicy target, PointPolicyUpdateRequest request);
 }
