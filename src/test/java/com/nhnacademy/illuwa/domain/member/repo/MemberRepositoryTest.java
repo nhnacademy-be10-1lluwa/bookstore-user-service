a50ffbe689df127a589cff7498e3d0639ff1a623
pear_c <zxcv9676@naver.com>
2025-06-18 17:43:49 +0900

    fix: test coverage 테스트

--- src/test/java/com/nhnacademy/illuwa/domain/member/repo/MemberRepositoryTest.java
+++ src/test/java/com/nhnacademy/illuwa/domain/member/repo/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
