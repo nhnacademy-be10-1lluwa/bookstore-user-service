be1c6ce1896dcf1345be7c50dae27759fd97f005
CGE <bestofGE@naver.com>
2025-06-18 09:11:21 +0900

    feat: guest service, controller 구현

--- src/main/java/com/nhnacademy/illuwa/domain/member/exception/MemberNotFoundException.java
+++ src/main/java/com/nhnacademy/illuwa/domain/member/exception/MemberNotFoundException.java
@@ -1,7 +1,7 @@
 package com.nhnacademy.illuwa.domain.member.exception;
 
 public class MemberNotFoundException extends RuntimeException {
-    public MemberNotFoundException(String message) {
-        super(message);
+    public MemberNotFoundException(Long memberId) {
+        super("회원을 찾을 수 없습니다: " + memberId);
     }
 }
