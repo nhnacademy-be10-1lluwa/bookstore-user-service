4bf5e34646111d951313e908ba8d55f0f207cece
pear_c <zxcv9676@naver.com>
2025-07-09 08:22:32 +0900

    feat: url 수정

--- src/main/java/com/nhnacademy/illuwa/domain/member/controller/SocialMemberController.java
+++ src/main/java/com/nhnacademy/illuwa/domain/member/controller/SocialMemberController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/internal/social-members")
+@RequestMapping("/members/internal/social-members")
 @RequiredArgsConstructor
 public class SocialMemberController {
     private final SocialMemberService socialMemberService;
