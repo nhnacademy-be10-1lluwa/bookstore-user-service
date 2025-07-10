30406e0d1503a1c165191899f0ece30d66fb9244
GAEUN <82525776+bestofGE@users.noreply.github.com>
2025-07-10 14:49:30 +0900

    refactor: fix api url (#179)

--- src/main/java/com/nhnacademy/illuwa/domain/member/controller/MemberController.java
+++ src/main/java/com/nhnacademy/illuwa/domain/member/controller/MemberController.java
@@ -22,13 +22,13 @@ public class MemberController {
     private final MemberService memberService;
 
     // 회원 목록 조회
-    @GetMapping("/admin/members")
+    @GetMapping("/api/admin/members")
     public ResponseEntity<List<MemberResponse>> getMemberList() {
         return ResponseEntity.status(HttpStatus.OK).body(memberService.getAllMembers());
     }
 
     // 회원 목록 조회
-    @GetMapping("/admin/members/paged")
+    @GetMapping("/api/admin/members/paged")
     public ResponseEntity<Page<MemberResponse>> getPagedMemberList(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
@@ -39,7 +39,7 @@ public class MemberController {
     }
 
     // 회원가입
-    @PostMapping("/members")
+    @PostMapping("/api/members")
     public ResponseEntity<MemberResponse> register(@Valid @RequestBody MemberRegisterRequest request) {
         MemberResponse saved = memberService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -47,19 +47,19 @@ public class MemberController {
     }
 
     // 로그인
-    @PostMapping("/members/login")
+    @PostMapping("/api/members/login")
     public ResponseEntity<MemberResponse> login(@Valid @RequestBody MemberLoginRequest request) {
         return ResponseEntity.status(HttpStatus.OK).body(memberService.login(request));
     }
 
     // 회원 단일 조회
-    @GetMapping("/members")
+    @GetMapping("/api/members")
     public ResponseEntity<MemberResponse> getMember(@RequestHeader("X-USER-ID") long memberId) {
         return ResponseEntity.status(HttpStatus.OK).body(memberService.getMemberById(memberId));
     }
 
     // 회원 수정
-    @PutMapping("/members")
+    @PutMapping("/api/members")
     public ResponseEntity<MemberResponse> updateMember(@RequestHeader("X-USER-ID") long memberId, @Valid @RequestBody MemberUpdateRequest request) {
         MemberResponse updatedMemberDto = memberService.updateMember(memberId, request);
         return ResponseEntity.status(HttpStatus.OK)
@@ -67,7 +67,7 @@ public class MemberController {
     }
 
     // 회원 비밀번호 체크
-    @PostMapping("/members/check-pw")
+    @PostMapping("/api/members/check-pw")
     public ResponseEntity<Boolean> checkPassword(@RequestHeader("X-USER-ID") long memberId, @RequestBody PasswordCheckRequest request) {
         log.info("비밀번호 체크 요청 - memberId: {}, password: {}", memberId, request.getInputPassword());
         boolean isEqual = memberService.checkPassword(memberId, request.getInputPassword());
@@ -76,13 +76,13 @@ public class MemberController {
     }
 
     //회원 삭제
-    @DeleteMapping("/members")
+    @DeleteMapping("/api/members")
     public void deleteMember(@RequestHeader("X-USER-ID") long memberId) {
         memberService.removeMember(memberId);
     }
 
     // 이번 달 생일 회원 조회
-    @GetMapping(value = "/members/birth-month", params = "month")
+    @GetMapping(value = "/api/members/birth-month", params = "month")
     public ResponseEntity<List<MemberResponse>> getMemberByBirthMonth(@RequestParam int month) {
         return ResponseEntity.ok(memberService.getMembersByBirthMonth(month));
     }
