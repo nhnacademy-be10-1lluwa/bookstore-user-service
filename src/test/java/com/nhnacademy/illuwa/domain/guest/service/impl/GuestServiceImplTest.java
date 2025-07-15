7a838f01bc6eb4bf82aa5b01697bfcbbe0fbd7c9
CGE <bestofGE@naver.com>
2025-07-16 01:59:21 +0900

    test: 회원등급,소셜멤버 Controller 테스트코드 작성

--- src/test/java/com/nhnacademy/illuwa/domain/guest/service/impl/GuestServiceImplTest.java
+++ src/test/java/com/nhnacademy/illuwa/domain/guest/service/impl/GuestServiceImplTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
@@ -26,6 +27,9 @@ class GuestServiceImplTest {
 
     @Mock
     GuestRepository guestRepository;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
 
     @InjectMocks
     GuestServiceImpl guestService;
