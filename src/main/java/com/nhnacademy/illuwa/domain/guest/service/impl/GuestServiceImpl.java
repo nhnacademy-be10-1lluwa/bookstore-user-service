919ebeb8f22adbb774f612eeb0430b07338c100b
CGE <bestofGE@naver.com>
2025-07-16 01:38:51 +0900

    feat: 결제비밀번호 암호화

--- src/main/java/com/nhnacademy/illuwa/domain/guest/service/impl/GuestServiceImpl.java
+++ src/main/java/com/nhnacademy/illuwa/domain/guest/service/impl/GuestServiceImpl.java
@@ -9,6 +9,7 @@ import com.nhnacademy.illuwa.domain.guest.repo.GuestRepository;
 import com.nhnacademy.illuwa.domain.guest.service.GuestService;
 import com.nhnacademy.illuwa.domain.message.service.MessageSendService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class GuestServiceImpl implements GuestService {
     private final GuestRepository guestRepository;
+    private final PasswordEncoder passwordEncoder;
     private final MessageSendService messageSendService;
 
     @Override
@@ -25,6 +27,7 @@ public class GuestServiceImpl implements GuestService {
                 .guestId(request.getGuestId())
                 .orderId(request.getOrderId())
                 .orderNumber(request.getOrderNumber())
+                .orderPassword(passwordEncoder.encode(request.getOrderPassword()))
                 .name(request.getName())
                 .email(request.getEmail())
                 .contact(request.getContact())
