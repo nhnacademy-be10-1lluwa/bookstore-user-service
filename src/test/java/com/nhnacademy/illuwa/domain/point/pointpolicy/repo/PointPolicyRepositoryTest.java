076220cf357f39d59685944d6b6897d7102d6e01
CGE <bestofGE@naver.com>
2025-07-24 17:21:10 +0900

    cleanup: guestController @Operations 추가,unused import 제거

--- src/test/java/com/nhnacademy/illuwa/domain/point/pointpolicy/repo/PointPolicyRepositoryTest.java
+++ src/test/java/com/nhnacademy/illuwa/domain/point/pointpolicy/repo/PointPolicyRepositoryTest.java
@@ -2,7 +2,6 @@ package com.nhnacademy.illuwa.domain.point.pointpolicy.repo;
 
 import com.nhnacademy.illuwa.domain.point.pointpolicy.entity.PointPolicy;
 import com.nhnacademy.illuwa.domain.point.pointpolicy.entity.enums.PointValueType;
-import com.nhnacademy.illuwa.domain.point.pointpolicy.repo.PointPolicyRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.*;
